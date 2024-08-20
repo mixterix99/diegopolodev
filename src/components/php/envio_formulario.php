102a06957c8b01a8e46b70abe7c59d2158c7fef4
Diego Polo <diegopolo14@gmail.com>
2024-08-19 22:45:52 -0500

    prueba de formulario

---
+++ src/components/php/envio_formulario.php
@@ -0,0 +1,43 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $secretKey = "6Ld9yyoqAAAAAE4JTwcuMgSJiahW1Smfpxz9uv6l";
+    $responseKey = $_POST['g-recaptcha-response'];
+    $userIP = $_SERVER['REMOTE_ADDR'];
+
+    // Verificar la respuesta del Captcha
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$userIP");
+    $responseKeys = json_decode($response, true);
+
+    if(intval($responseKeys["success"]) !== 1) {
+        echo "Por favor, verifica que no eres un robot.";
+    } else {
+        // Captura y sanitiza los datos del formulario
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Correo electrónico no válido.";
+            exit;
+        }
+
+        $phone = htmlspecialchars($_POST['phone']);
+        $country = htmlspecialchars($_POST['country']);
+        $asunto = htmlspecialchars($_POST['subject']);
+        $mensaje = htmlspecialchars($_POST['message']);
+
+        // Configuración del correo
+        $to = "info@diegopolodev.com";
+        $subject = $asunto;
+        $message = "Correo: $email\nTeléfono: $phone\nPaís: $country\nMensaje: $mensaje";
+        $headers = "From: $email\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        // Enviar el correo
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Mensaje enviado correctamente.";
+        } else {
+            echo "Error al enviar el mensaje.";
+        }
+    }
+}
+?>
